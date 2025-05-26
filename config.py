49f597c0cd19491084fb1481eb8e05b6d7bf7d8d
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-05-27 02:26:53 +0500

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_IDS = [123456789]  # Replace with real admin ID(s)
+BOT_TOKEN = "7348821242:AAEYxu9eXAJjV9mbovPkYO5oaLQBMeRNmwo"
+ADMIN_IDS = [7646506662]  # Replace with real admin ID(s)
